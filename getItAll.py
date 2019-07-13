1afb153caeedc650454731a7950fde25b821b0e4
Rémi Côme <remi.come@univ-lorraine.fr>
2019-07-13 20:11:51 +0200

    Récupère les uris des images

--- getItAll.py
+++ getItAll.py
@@ -7,13 +7,15 @@ test_url="https://rewildingeurope.com/what-is-rewilding/"
 import requests
 from bs4 import BeautifulSoup
 
+import re
+
+
+# On ouvre la page et on la charge dans le parser
 html = requests.get(test_url).content
 soup = BeautifulSoup(html, 'html.parser')
 
-
 # On n'a besoin que du tag <main>
 main = soup.main
-
 
 #Specifique au site de rewilding Europe: on enlève de l'arbre le <div> qui correspond à l'image de fond devant s'afficher sur la version mobile
 def is_for_mobile(tag):
@@ -33,3 +35,19 @@ for text in main.find_all(["h1","h2","h3","h4","h5","h6","p"]):
    print(text.get_text())
 
 
+## Récupère toutes les images de la page
+images=[]
+
+for tag in main.find_all(True):
+    # 1ere possibilité: l'image est définie par CSS comme image de fond
+    if tag.has_attr('style'):
+        s = re.search('background-image:url\(\'(.*\.jpg)\'', tag['style'])
+        if s:
+            images.append(s.group(1))
+
+    # 2e possibilité: balise <img>
+    if (tag.name == 'img') and tag.has_attr('src'):
+        images.append(tag['src'])
+
+
+print(len(images))
